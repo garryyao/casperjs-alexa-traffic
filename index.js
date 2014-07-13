8ec3c5b6bd140790c223a81d6fa5961fddf849ee
Garry Yao <yaojun85@gmail.com>
2014-07-13 11:58:16 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env casperjs
+// Simple scrapper that read traffic from http://www.alexa.cn/?url=[query]
+// [NOTE] This is not node script but phantom!
+
+var args = require('system').args;
+var site = args[args.length-1];
+var query = "http://www.alexa.cn/?url=" + site;
+var numeral = require('numeral');
+
+var fs = require('fs');
+var path = require('path');
+var jquery_file = path.resolve(fs.workingDirectory, 'node_modules/jquery/dist/jquery.min.js');
+
+var casper = require('casper').create({
+  clientScripts: [jquery_file]
+});
+
+console.log("querying for traffic of...", site)
+
+// load the website page
+casper.start(query, function onPageLoad(status) {
+  this.echo(this.evaluate(function clientSideCode() {
+    return $(".info2.liw0").text();
+  }).replace(/\d+/g, function(traffic){
+    return numeral(+traffic).format('0a');
+  }));
+});
+
+casper.run(function(){
+  this.exit();
+});
